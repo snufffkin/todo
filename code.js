d560206f6cdf05864e54a2a2cbd88234a2f527a5
isakura313 <isakura313@gmail.com>
2019-01-10 22:54:11 +0300

    add local storage and etc

--- code.js
+++ code.js
@@ -1,36 +1,60 @@
-
-  var motivation_array = ['К черту все! Берись и делай!',
+//здесь мы задаем массив с фразами, которые должны нас мотивировать
+let motivation_array = ['К черту все! Берись и делай!',
 'Если ты не знаешь, чего хочешь, ты в итоге останешься с тем, чего точно не хочешь',
 'Все победы начинаются с победы над самим собой',
-'Это своего рода забава, делать невозможное','"Неважно, кто мы такие, важно, какой у нас план'];
-  //здесь мы задаем массив с фразами, которые должны нас мотивировать
+'Это своего рода забава, делать невозможное','Неважно, кто мы такие, важно, какой у нас план'];
 
 
-  let button = document.querySelector('.button_plus');
-  let tasks = document.querySelector('.tasks');
-  let field = document.querySelector('input');
-  let logo = document.querySelector('.logo');
+
+let button = document.querySelector('.button_plus');
+let tasks = document.querySelector('.tasks');
+let field = document.querySelector('input');
+let logo = document.querySelector('.logo');
 
 
-  // здесь у нас происходит смена цитат
-  function changePhrase() {
-    document.querySelector('.motivation_speech').innerHTML = motivation_array[Math.round(Math.random()*(motivation_array.length - 1))];
-    logo.className = "logo animated flash slow";
+// здесь у нас происходит смена цитат
+function changePhrase() {
+  document.querySelector('.motivation_speech').innerHTML = motivation_array[Math.round(Math.random()*(motivation_array.length - 1))];
+  logo.className = "logo animated flash slow";
+    //здесь надо переписать через addClass
   }
   //запуск смены фраз;
-  setInterval(changePhrase, 8000);
-  setInterval(function(){
-    logo.className = "logo";
-  }, 19000);
+setInterval(changePhrase, 8000);
 //запуск смены лого
+setInterval(function(){
+  logo.className = "logo";
+}, 19000);
+
+
+
+function DrawonLoad(){
+  for(key in localStorage)
+  if(key.charAt(0)=="+"){
+  key = key.substring(1);
+  let index = Math.round(Math.random()*27);
+    tasks.insertAdjacentHTML('afterbegin', `<div class='wrap-task'><div class="task">
+    <img src="monsters/svg/monster-${index}.svg">
+    <p>${key}</p>
+  </div>
+  <i class="fas fa-trash-alt trash"></i>
+  </div>
+  `);
+  field.value = '';
+  }
+}
+DrawonLoad();
+
+
 
 //функция создания нового дела
 function createItem(){
-    let text = field.value;
+  let text = field.value;
     if (!text) {
-      return;
-      //здесь проверяем на наличие пустого поля
+      return; 
+      //return сразу прекращает выполнение функции
     }
+    let x ="+"+text;
+    localStorage.setItem(x, x);
     let index = Math.round(Math.random()*27);
     tasks.insertAdjacentHTML('afterbegin', `<div class='wrap-task'><div class="task">
     <img src="monsters/svg/monster-${index}.svg">
@@ -69,7 +93,11 @@ tasks.addEventListener('click', function (event) {
   item2.className = "animated flipOutX wrap-task";
   setTimeout(function(){
     item2.parentNode.removeChild(item2);
-    
+    let x = item2.textContent;
+    x = x.trim();
+    //трим вырезает лишние пробелы
+    x = "+"+x;
+    localStorage.removeItem(x);
   }, 700)
 });
   
